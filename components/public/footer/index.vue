fde6e7d65f63e0c1e97647696117f77cae89ace2
王嘉炀 <wangjiayang@tv189.com>
2018-11-09 10:39:08 +0800

    增加备案号

--- components/public/footer/index.vue
+++ components/public/footer/index.vue
@@ -3,12 +3,10 @@
     <div class="footer-content">
       <div class="footer-copyright clearfix">
         <div class="footer-copyright-left">
-          <p>Copyright©2018 王嘉炀个人博客 wangjiayang.cn <a href="http://www.beianbeian.com/beianxinxi/283f39a9-4c00-427a-97ef-3c7a9e1e0af1.html">苏ICP备14039042号</a></p>
+          <p>Copyright©2018 王嘉炀个人博客 wangjiayang.cn</p>
+          <p><a href="http://www.miitbeian.gov.cn/publish/query/indexFirst.action">沪ICP备18041792号</a></p>
         </div>
         <div class="footer-copyright-right">
-          <!-- <a
-            href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010502025545"
-            target="_blank">京公网安备11010502025545号</a> -->
           <div class="footer-copyright-cert"><a
             class="sp-ft sp-ft--record"
             href="https://s.click.taobao.com/Kb3GbKw"

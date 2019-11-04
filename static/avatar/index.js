ce9fd8afab2fd8469d0daabbdf4e72a22b9ff3e8
王嘉炀 <346461062@qq.com>
2019-11-04 14:59:35 +0800

    update

--- static/avatar/index.js
+++ static/avatar/index.js
@@ -2,65 +2,66 @@
 
 var canvas = document.getElementById('canvas')
 var context = canvas.getContext('2d')
-var  devicePixelRatio = window.devicePixelRatio || 1,   
-  backingStoreRatio = context.webkitBackingStorePixelRatio || context.mozBackingStorePixelRatio || context.msBackingStorePixelRatio || context.oBackingStorePixelRatio || context.backingStorePixelRatio || 1, 
-  ratio = devicePixelRatio / backingStoreRatio;
+var devicePixelRatio = window.devicePixelRatio || 1
+var backingStoreRatio = context.webkitBackingStorePixelRatio || context.mozBackingStorePixelRatio || context.msBackingStorePixelRatio || context.oBackingStorePixelRatio || context.backingStorePixelRatio || 1
+var ratio = devicePixelRatio / backingStoreRatio
 var canvasWeight = 200
 var canvasHeight = 200
 // canvas.width = canvasWeight
 // canvas.height = canvasHeight
-canvas.width = canvasWeight * ratio; 
-canvas.height = canvasHeight * ratio; 
+canvas.width = canvasWeight * ratio
+canvas.height = canvasHeight * ratio
 
-canvas.style.width = canvasWeight + 'px'; 
-canvas.style.height = canvasHeight + 'px'; 
+canvas.style.width = canvasWeight + 'px'
+canvas.style.height = canvasHeight + 'px'
 
-context.scale(ratio, ratio);
+context.scale(ratio, ratio)
 
-var image = new Image();
+var image = new Image()
 // image.crossOrigin="anonymous"
 image.src = 'img/headicon.jpeg'
-image.onload = function(){
+image.onload = function () {
+  // console.log(image)
+  document.getElementById('avatar').src = image.src
   initCanvas()
 }
-function initCanvas(){
+function initCanvas () {
   context.clearRect(0, 0, canvasWeight, canvasHeight)
   draw(image)
 }
-function draw(){
+function draw () {
   // context.save()
   context.drawImage(image, 0, 0, canvasWeight, canvasHeight)
   // context.restore()
 }
 
 // 保存成png格式的图片
-function saveAsPNG() {
-  return canvas.toDataURL("image/png");
+function saveAsPNG () {
+  return canvas.toDataURL('image/png')
 }
 // 下载
-function downLoad(url){
-  var oA = document.createElement("a");
-  oA.download = '';// 设置下载的文件名，默认是'下载'
-  oA.href = url;
-  document.body.appendChild(oA);
-  oA.click();
-  oA.remove(); // 下载之后把创建的元素删除
+function downLoad (url) {
+  var oA = document.createElement('a')
+  oA.download = ''// 设置下载的文件名，默认是'下载'
+  oA.href = url
+  document.body.appendChild(oA)
+  oA.click()
+  oA.remove() // 下载之后把创建的元素删除
 }
 
-
-document.getElementById('headfixed').onclick =function(){
+document.getElementById('headfixed').onclick = function () {
   console.log(this)
   initCanvas()
   context.drawImage(this, 0, 0, canvasWeight, canvasHeight)
+  document.getElementById('avatar').src = saveAsPNG()
 }
-document.getElementById('headfixed2').onclick =function(){
+document.getElementById('headfixed2').onclick = function () {
   console.log(this)
   initCanvas()
   context.drawImage(this, 0, 0, canvasWeight, canvasHeight)
+  document.getElementById('avatar').src = saveAsPNG()
 }
 
-
-document.getElementById('saveimg').onclick = function(){
-  downLoad(saveAsPNG(canvas));
+document.getElementById('saveimg').onclick = function () {
+  downLoad(saveAsPNG(canvas))
 }
-
